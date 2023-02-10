3c0cb846a6b00219bc19a7d10b5446dcca8b6083
NickIsBased <nicholasjholbrook@gmail.com>
2023-02-10 18:56:18 -0500

    got navx code has not been tested

--- src/main/java/frc/robot/commands/DriveWithJoystickCommand.java
+++ src/main/java/frc/robot/commands/DriveWithJoystickCommand.java
@@ -4,18 +4,22 @@
 
 package frc.robot.commands;
 
+import com.kauailabs.navx.frc.AHRS;
+
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-
-//import java.rmi.server.RemoteObjectInvocationHandler;
-
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.RobotContainer;
 import frc.robot.subsystems.DrivetrainSubsystem;
+//import java.rmi.server.RemoteObjectInvocationHandler;
+
+
 
 public class DriveWithJoystickCommand extends CommandBase {
   @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
   private final DrivetrainSubsystem drivetrainSubsystem;
   int m_val ;
+  public int uwu = 69;
+  AHRS ahrs;
 
   public DriveWithJoystickCommand(DrivetrainSubsystem drivetrainSubsystem){
     this.drivetrainSubsystem = drivetrainSubsystem;
@@ -64,40 +68,38 @@ public class DriveWithJoystickCommand extends CommandBase {
       coastnum ++;
       if (coastnum % 2 == 0) {
         drivetrainSubsystem.setCoastMode();
+        SmartDashboard.putString("coast or break?", "coast");
       }
       else{
         drivetrainSubsystem.setBreakMode();
+        SmartDashboard.putString("coast or break?", "break");
       }
     }
     //sets sqrt
     if (RobotContainer.controller.getBButtonPressed() == true) {
       m_val = 8;
-      SmartDashboard.putString("B button", "using sqrt");
-      SmartDashboard.putString("A button", "not in use");
-      SmartDashboard.putString("X button", "not in use");
+      SmartDashboard.putString("mode", "using sqrt");
     }
     //changes input output ratio to be exponential
     else if (RobotContainer.controller.getAButtonPressed() == true) {
       m_val = 10;
-      SmartDashboard.putString("A button", "using expo");
-      SmartDashboard.putString("B button", "not in use");
-      SmartDashboard.putString("X button", "not in use");
+      SmartDashboard.putString("mode", "using expo");
     }
     //changes input output ratio to be cooler exponential(y = 2^x - 1 0r y = -2^-x - 1)
     else if (RobotContainer.controller.getXButtonPressed() == true) {
       m_val = 12;
-      SmartDashboard.putString("X button", "using cool expo");
-      SmartDashboard.putString("B button", "not in use");
-      SmartDashboard.putString("A button", "not in use");
+      SmartDashboard.putString("mode", "using cool expo");
     }
     //resets so input = output
-    if (RobotContainer.controller.getLeftBumperPressed() == true) {
+    else if (RobotContainer.controller.getLeftBumperPressed() == true) {
       m_val = 0;
-      SmartDashboard.putString("X button", "not in use");
-      SmartDashboard.putString("B button", "not in use");
-      SmartDashboard.putString("A button", "not in use");
+      SmartDashboard.putString("mode", "not in use");
     }
-
+    else if (RobotContainer.controller.getRightBumper() == true) {
+      uwu = 42;
+      SmartDashboard.putString("mode", "balancing");
+      m_val = 16;
+    }
 
     //sqrt function
     if (m_val == 8){
@@ -105,6 +107,13 @@ public class DriveWithJoystickCommand extends CommandBase {
       rForwardSpeed = formulas(rForwardSpeed);
       SmartDashboard.putNumber("Left speed", lForwardSpeed);
       SmartDashboard.putNumber("right speed", rForwardSpeed);
+    }
+    else if (m_val == 16){
+      lForwardSpeed = 0;
+      rForwardSpeed = 0;
+      SmartDashboard.putNumber("left speed", 69);
+      SmartDashboard.putNumber("right speed", 69);
+      balancepwease();
     }
     //normal exponential (x^3) function
     else if (m_val == 10) {
@@ -150,6 +159,18 @@ public class DriveWithJoystickCommand extends CommandBase {
       value = Math.pow(-2, (value * -1)) + 1;
     }
     return value;
+  }
+
+  private void balancepwease() {
+    double navXPitch = ahrs.getPitch();
+    double navXRoll = ahrs.getRoll();
+    if (navXPitch > 10){
+      drivetrainSubsystem.tankDrive(-0.2, -0.2);
+    }
+    if (navXPitch < -10) {
+      drivetrainSubsystem.tankDrive(0.2, 0.2);
+    }
+
   }
 
   //tank drive, comment one out
