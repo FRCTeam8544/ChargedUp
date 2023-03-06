9f6bb4e26daf373ee98db6a918b56eedd9e84375
nfurno <nicholas.furno@gmail.com>
2023-03-06 18:03:05 -0500

    removed markings from merge resolution

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -1,10 +1,7 @@
 package frc.robot.subsystems;
 
-<<<<<<< HEAD
-=======
 import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import edu.wpi.first.wpilibj.motorcontrol.Victor;
->>>>>>> main
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
