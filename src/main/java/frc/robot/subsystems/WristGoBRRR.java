dd4545cf44a1f1d359c163f718a5ae4f353025ca
NickIsBased <nicholasjholbrook@gmail.com>
2023-07-05 19:34:30 -0400

    made a motor for the wheel intake optional

--- src/main/java/frc/robot/subsystems/WristGoBRRR.java
+++ src/main/java/frc/robot/subsystems/WristGoBRRR.java
@@ -8,26 +8,40 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class WristGoBRRR extends SubsystemBase{
+    boolean tokyoWorks = true, driftWorks = true;
+    VictorSPX tokyo;
+    VictorSPX drift;
 
-    VictorSPX tokyo = new VictorSPX(Constants.armthings.TOKYOID);
-    VictorSPX drift = new VictorSPX(Constants.armthings.DRIFTID);
-    
+
     public WristGoBRRR(){//I wonder if you know how they live in tokyo
-        tokyo.setInverted(true);
-        drift.setInverted(true);
+        try{
+            tokyo = new VictorSPX(Constants.armthings.TOKYOID);
+        }catch(Exception e){
+            tokyoWorks = false;
+        }
+    
+        try{
+            drift = new VictorSPX(Constants.armthings.DRIFTID);
+        } catch(Exception f) {
+            driftWorks = false;
+        }
 
+        if (tokyoWorks){tokyo.setInverted(true);}
+        if (driftWorks){drift.setInverted(true);}
+
+        
     }
 
     public void iveGotANeed(){
-        tokyo.set(ControlMode.PercentOutput, Constants.armthings.TOKYODRIFT);
-        drift.set(ControlMode.PercentOutput, Constants.armthings.TOKYODRIFT);
+        if (tokyoWorks){tokyo.set(ControlMode.PercentOutput, Constants.armthings.TOKYODRIFT);}
+        if (driftWorks){drift.set(ControlMode.PercentOutput, Constants.armthings.TOKYODRIFT);}
     }
     public void forSpeed(){
-        tokyo.set(ControlMode.PercentOutput, Constants.armthings.TOKYODRIFT * -1);
-        drift.set(ControlMode.PercentOutput, Constants.armthings.TOKYODRIFT * -1);
+        if (tokyoWorks){tokyo.set(ControlMode.PercentOutput, Constants.armthings.TOKYODRIFT * -1);}
+        if (driftWorks){drift.set(ControlMode.PercentOutput, Constants.armthings.TOKYODRIFT * -1);}
     }
     public void skrrttt(){
-        tokyo.set(ControlMode.PercentOutput, 0);
-        drift.set(ControlMode.PercentOutput, 0);
+        if (tokyoWorks){tokyo.set(ControlMode.PercentOutput, 0);}
+        if (driftWorks){drift.set(ControlMode.PercentOutput, 0);}
     }
 }
